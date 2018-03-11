cb55b6adab2cf0cace7ef7c56cf40d2cc9802aa0
G. Wade Johnson <gwadej@anomaly.org>
2018-03-11 17:19:10 -0500

    Link table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :blog
+
+  belongs_to :tag
+  has_many :tags
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ApplicationRecord
+
+  belongs_to :post
+  has_many :posts
 end
