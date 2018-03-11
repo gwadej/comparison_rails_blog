01e8ac10839658a5ff0f9300a2d2627b0a9ed8d7
G. Wade Johnson <gwadej@anomaly.org>
2018-03-11 17:28:16 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311221337) do
+ActiveRecord::Schema.define(version: 20180311221830) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "uuid"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20180311221337) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_blogs_on_user_id"
     t.index ["uuid"], name: "index_blogs_on_uuid", unique: true
+  end
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_tags_on_post_id"
+    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
   end
 
   create_table "posts", force: :cascade do |t|
