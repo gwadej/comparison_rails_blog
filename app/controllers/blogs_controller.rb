b3bc692761946acca8d512f9e3fd781a2c9f8d04
G. Wade Johnson <gwadej@anomaly.org>
2018-03-25 15:19:57 -0500

    Put together initial blog routes

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,34 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.order(:title)
   end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    # How do I set the 
+    @blog = Blog.create! uuid: Blog.uuid,
+      user: current_user,
+      title: params.require(:blog).require(:title),
+      description: params.require(:blog).permit(:description)
+
+    redirect_to(@blog)
+  end
+
+  def edit
+    # Form for modifying blog
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to(@blog)
+    else
+      render :edit
+    end
+  end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,9 @@
+require 'securerandom'
+
 class Blog < ApplicationRecord
   belongs_to :user
+
+  def self.uuid
+    SecureRandom.uuid.gsub(/-/, '')
+  end
 end
