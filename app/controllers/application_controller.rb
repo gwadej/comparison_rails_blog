35840996875e3a199ab69f4837ff02ee316d25c8
G. Wade Johnson <gwadej@anomaly.org>
2018-03-26 21:57:34 -0500

    Fix current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
+    return unless session && warden && warden.user
+
+    @current_user ||= User.find(warden.user.id)
   end
 end
