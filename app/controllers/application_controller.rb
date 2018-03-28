3d696209824f9f2e247a25f128d2659bb9346c3c
G. Wade Johnson <gwadej@anomaly.org>
2018-03-27 21:04:43 -0500

    Correct authentication code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def current_user
-    return unless session && warden && warden.user
-
-    @current_user ||= User.find(warden.user.id)
-  end
+  before_action :authenticate_user!
 end
